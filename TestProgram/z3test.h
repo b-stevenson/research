33a847df5e1a62962fb37dd0c396ee8f3c5486cd
Benjamin Stevenson <benstevenson93@gmail.com>
2015-04-13 15:30:20 -0500

    added header

--- TestProgram/z3test.h
+++ TestProgram/z3test.h
@@ -1,4 +1,13 @@
 #ifndef Z3TEST_H
 #define Z3TEST_H
+class z3Math: public SymbolicVar {
+public:
+    z3Math(string varName) : varName(varName) { }
+    string toString() const {return this->varName; }
+    string getName() const { return this->varName; }
 
+
+private:
+    expr e;
+};
 #endif // Z3TEST_H
